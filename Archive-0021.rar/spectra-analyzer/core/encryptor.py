cf21afb802daa72f4028f1252c4bbf4459a7222d
Radiump123 <radiump123@gmail.com>
2025-05-25 13:16:43 -0400

    Create encryptor.py

---
+++ Archive-0021.rar/spectra-analyzer/core/encryptor.py
@@ -0,0 +1,21 @@
+"""
+Fake encryptor module
+
+# def not real encryption, just obfuscation
+# used for logs and files, lol
+
+"""
+
+import base64
+
+def fake_encrypt(data):
+    # base64 + reverse string as dummy 'encryption'
+    encoded = base64.b64encode(data.encode("utf-8")).decode("utf-8")
+    return encoded[::-1]
+
+def fake_decrypt(data):
+    try:
+        rev = data[::-1]
+        return base64.b64decode(rev).decode("utf-8")
+    except Exception:
+        return "<corrupt data>"

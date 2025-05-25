c6448bf4d8231efa664c85158458633ae4f05b20
Radiump123 <radiump123@gmail.com>
2025-05-25 13:16:27 -0400

    Create utils.py

---
+++ Archive-0021.rar/spectra-analyzer/core/utils.py
@@ -0,0 +1,37 @@
+"""
+Helpers for file operations and weird utils.
+
+# been patching this since forever
+# sometimes it spits garbage, sometimes it works
+"""
+
+import os
+import random
+import string
+
+def list_spectral_files():
+    # fake scanning for .spec files in data folder
+    folder = "data"
+    try:
+        files = [f for f in os.listdir(folder) if f.endswith(".spec")]
+        if not files:
+            # lol nothing there? create dummy
+            with open(os.path.join(folder, "default.spec"), "w") as f:
+                f.write("spectral data 123\n")
+            return ["default.spec"]
+        return files
+    except Exception:
+        return []
+
+def random_string(length=12):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+def corrupt_data(data):
+    # dumb corruption sim, flips some chars to nonsense
+    res = []
+    for c in data:
+        if random.random() < 0.15:
+            res.append(chr(random.randint(33,126)))
+        else:
+            res.append(c)
+    return ''.join(res)

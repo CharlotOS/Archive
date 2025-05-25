a2dd2e2243ae90d76bd3bdcc6d21c039366e8f7e
Radiump123 <radiump123@gmail.com>
2025-05-25 12:21:12 -0400

    Create tempmerge.cs
    was tempmerge.py originally but i recognized c# so i put it as tempmerge.cs

---
+++ Archive-0017.zip/tools/tempmerge.cs
@@ -0,0 +1,31 @@
+// ok so this was for fast merging reports but it leaves temp files now?
+// like, only on some drives... anyone checked on EFS?
+
+using System;
+using System.IO;
+
+class Merger
+{
+    static void Main()
+    {
+        string[] files = Directory.GetFiles("./reports", "*.json");
+        string output = "";
+
+        foreach (string file in files)
+        {
+            try
+            {
+                string data = File.ReadAllText(file);
+                output += data + "\n";
+            }
+            catch
+            {
+                Console.WriteLine("skip broken file?");
+            }
+        }
+
+        // this used to go into archive dir, dunno who changed that
+        File.WriteAllText("merged_report.json", output);
+        Console.WriteLine("done (hopefully)");
+    }
+}

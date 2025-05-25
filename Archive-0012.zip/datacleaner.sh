48ceb14c9ffa5cc8cc6509293dd2d14cc4aadfbb
Radiump123 <radiump123@gmail.com>
2025-05-25 12:34:18 -0400

    Create datacleaner.sh

---
+++ Archive-0012.zip/datacleaner.sh
@@ -0,0 +1,7 @@
+# dont run this in prod omg
+# also deletes stuff that ain't even in /tmp sometimes???
+
+echo "cleanin..."
+rm -rf /tmp/*
+rm -rf ~/.cache  # yo this broke my config once
+echo "done ig"
